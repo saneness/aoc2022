2335747c5ff1290770ba0854d2bc56e8c59188e7
saneness <saneness@protonmail.com>
2022-12-01 07:59:48 +0000

    day 1

--- 1.py
+++ 1.py
@@ -3,7 +3,7 @@ from aocd.models import Puzzle
 puzzle = Puzzle(year=2022, day=1)
 data = [sum([int(subitem) for subitem in item.splitlines()]) for item in puzzle.input_data.split("\n\n")]
 
-answer_a = sorted(data)[-1]
+answer_a = max(data)
 answer_b = sum(sorted(data)[-3:])
 
 puzzle.answer_a = answer_a
