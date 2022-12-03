b3de5736d07248b79a58c1bad8dc87ce55c6ed48
saneness <saneness@protonmail.com>
2022-12-03 10:56:10 +0000

    day 3

--- 3.py
+++ 3.py
@@ -3,20 +3,17 @@ from aocd.models import Puzzle
 puzzle = Puzzle(year=2022, day=3)
 data = puzzle.input_data.splitlines()
 
-def share(items):
-    result = items[0]
-    for i in range(1, len(items)):
-        result = result.intersection(items[i])
-    return ''.join(result)
+def intersection(items):
+    return ''.join([''.join(set.intersection(*item)) for item in items])
 
 def priority(items):
     return sum([ord(char) - 96 if ord(char) > 96 else ord(char) - 38 for char in items])
 
 compartments = [[set(item[:len(item)//2]), set(item[len(item)//2:])] for item in data]
-answer_a = priority(''.join([share(items) for items in compartments]))
+answer_a = priority(intersection(compartments))
 
 groups = [[set(item) for item in data[i:i+3]] for i in range(0, len(data), 3)]
-answer_b = priority(''.join([share(rucksacks) for rucksacks in groups]))
+answer_b = priority(intersection(groups))
 
 puzzle.answer_a = answer_a
 puzzle.answer_b = answer_b
