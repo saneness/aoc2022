e716c2a285f75cf2bbd3279c35f2d1908f5e3fcb
saneness <saneness@protonmail.com>
2022-12-08 11:53:04 +0000

    day 8

--- 08.py
+++ 08.py
@@ -5,16 +5,12 @@ import math
 puzzle = Puzzle(year=2022, day=8)
 data = np.array([[*line] for line in puzzle.input_data.splitlines()]).astype(int)
 
-def is_visible(trees, i, j):
-    sides = [[tree >= trees[i][j] for tree in side] for side in [trees[i, :j], trees[i, j+1:], trees[:i, j], trees[i+1:, j]]]
-    return not math.prod([sum(side) for side in sides]), math.prod([((side[::-1] if sides.index(side) % 2 == 0 else side)[:-1] + [1]).index(1) + 1 for side in sides])
-
 visible_to = 4 * (len(data) - 1)
 visible_from = 0
 for (i, j), tree in np.ndenumerate(data[1:-1, 1:-1]):
-    _to, _from = is_visible(data, i + 1, j + 1)
-    visible_to += _to
-    visible_from = max(visible_from, _from)
+    sides = [[tree >= data[i+1][j+1] for tree in side] for side in [data[i+1, :j+1], data[i+1, j+2:], data[:i+1, j+1], data[i+2:, j+1]]]
+    visible_to += not math.prod([sum(side) for side in sides])
+    visible_from = max(visible_from, math.prod([((side[::-1] if sides.index(side) % 2 == 0 else side)[:-1] + [1]).index(1) + 1 for side in sides]))
 
 answer_a = visible_to
 answer_b = visible_from
