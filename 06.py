ce137e105c62bc82acedf0e6e256d65b27c2fd1e
saneness <saneness@protonmail.com>
2022-12-12 12:12:24 +0000

    day 6

--- 06.py
+++ 06.py
@@ -6,8 +6,8 @@ data = puzzle.input_data
 def marker(message, n):
     return min([i + n for i in range(len(message)-n) if len(set(message[i:i+n])) == n])
 
-answer_a = marker(data, 4)
-answer_b = marker(data, 14)
+answer_a = marker(message=data, n=4)
+answer_b = marker(message=data, n=14)
 
 puzzle.answer_a = answer_a
 puzzle.answer_b = answer_b
